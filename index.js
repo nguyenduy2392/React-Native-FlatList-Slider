1b16a5f9c53077987dda5960721bdf1f25ec2c5e
nguyenduy2392 <nguyenduy2392@gmail.com>
2021-01-19 16:44:41 +0700

    change to lib

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-/**
- * @format
- */
+import FlatListSlider from './src/FlatListSlider';
+import Indicator from './src/Indicator';
 
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-
-import FlatListSlider from "./src/FlatListSlider";
-
-export default FlatListSlider;
+export { FlatListSlider, Indicator };
